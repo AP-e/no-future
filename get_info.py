510043cf12754c4e4e791cd7224e73299201b5d8
AP-e <afrophysics@live.co.uk>
2018-01-04 20:09:48 +0000

    Implement basic Discogs release searching

--- get_info.py
+++ get_info.py
@@ -1,6 +1,7 @@
 """ get_info """
 import os
 import discogs_client
+import re
 import ratelimit
 
 DISCOGS_USER_AGENT = 'nofuture/0.1'
@@ -17,3 +18,23 @@ def make_client():
     if user_token is None:
             raise ValueError('You must specify a valid discogs user token')
     return discogs_client.Client(DISCOGS_USER_AGENT, user_token=user_token)
+
+def find_release(client, dirname):
+    """Return Discogs release matching directory name."""
+    full_title, tags = split_dirname(dirname)
+    releases = client.search(full_title, type='release')
+    try:
+        return releases[0] # assume first result is best match
+    except IndexError:
+        return None
+
+def split_dirname(dirname):
+    """Split dirname into full title string and list of debracketed tags."""
+    p = re.compile(r'\[\w+\]')
+    m = p.search(dirname)
+    if m is None:
+        full_title, tags = dirname, []
+    else:
+        full_title = dirname[:m.start()].strip()
+        tags = [tag.strip('[]') for tag in p.findall(dirname, m.start())]
+    return full_title, tags
