98ba3e27d41b3205e265bcef3747085498744403
AP-e <afrophysics@live.co.uk>
2018-02-04 17:16:30 +0000

    Write basic setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='nofuture',
+      version='0.1',
+      description='Automatically decompress and organise music downloaded from nodata',
+      url='https://github.com/AP-e/nofuture',
+      packages=['nofuture'],
+     )
